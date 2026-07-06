010e2ffbf8d92bf1ac6d9a761b2928824b66e471
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:41 -0300

    fixes

--- src/flext_tap_oracle/_utilities/__init__.py
+++ src/flext_tap_oracle/_utilities/__init__.py
@@ -8,9 +8,7 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tap_oracle._utilities.client import (
-        FlextTapOracleUtilitiesClientMixin as FlextTapOracleUtilitiesClientMixin,
-    )
+    from flext_tap_oracle._utilities.client import FlextTapOracleUtilitiesClientMixin
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".client": ("FlextTapOracleUtilitiesClientMixin",),
