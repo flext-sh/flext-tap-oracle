acfb700ced14fbd147e22f679ac644aef6b091bf
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 11:26:36 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- src/flext_tap_oracle/__init__.py
+++ src/flext_tap_oracle/__init__.py
@@ -9,12 +9,19 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 from .__version__ import (
-    __author__ as __author__, __author_email__ as __author_email__, __description__ as __description__, __license__ as __license__,
-    __title__ as __title__, __url__ as __url__, __version__ as __version__, __version_info__ as __version_info__,
+    __author__ as __author__,
+    __author_email__ as __author_email__,
+    __description__ as __description__,
+    __license__ as __license__,
+    __title__ as __title__,
+    __url__ as __url__,
+    __version__ as __version__,
+    __version_info__ as __version_info__,
 )
 
 if TYPE_CHECKING:
-    from flext_db_oracle import d, e, h, r, s, x
+    from flext_core import d, h, r, x
+    from flext_db_oracle import e, s
 
     from .__version__ import FlextTapOracleVersion
     from ._config import FlextTapOracleConfig, config
@@ -27,14 +34,38 @@ if TYPE_CHECKING:
     from .typings import FlextTapOracleTypes, FlextTapOracleTypes as t
     from .utilities import FlextTapOracleUtilities, FlextTapOracleUtilities as u
 __all__: tuple[str, ...] = (
-    "FlextTapOracleConfig", "FlextTapOracleConstants", "FlextTapOracleModels", "FlextTapOracleProtocols",
-    "FlextTapOracleService", "FlextTapOracleSettings", "FlextTapOracleStreams", "FlextTapOracleTypes",
-    "FlextTapOracleUtilities", "FlextTapOracleVersion", "__author__", "__author_email__",
-    "__description__", "__license__", "__title__", "__url__",
-    "__version__", "__version_info__", "c", "config",
-    "d", "e", "h", "m",
-    "p", "r", "s", "settings",
-    "t", "tap_oracle", "u", "x",
+    "FlextTapOracleConfig",
+    "FlextTapOracleConstants",
+    "FlextTapOracleModels",
+    "FlextTapOracleProtocols",
+    "FlextTapOracleService",
+    "FlextTapOracleSettings",
+    "FlextTapOracleStreams",
+    "FlextTapOracleTypes",
+    "FlextTapOracleUtilities",
+    "FlextTapOracleVersion",
+    "__author__",
+    "__author_email__",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__url__",
+    "__version__",
+    "__version_info__",
+    "c",
+    "config",
+    "d",
+    "e",
+    "h",
+    "m",
+    "p",
+    "r",
+    "s",
+    "settings",
+    "t",
+    "tap_oracle",
+    "u",
+    "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
@@ -50,7 +81,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".streams": ("FlextTapOracleStreams",),
             ".typings": ("FlextTapOracleTypes", "t"),
             ".utilities": ("FlextTapOracleUtilities", "u"),
-            "flext_db_oracle": ("d", "e", "h", "r", "s", "x"),
+            "flext_core": ("d", "h", "r", "x"),
+            "flext_db_oracle": ("e", "s"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
